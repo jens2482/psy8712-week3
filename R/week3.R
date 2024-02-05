3e871fd96341862a9453a2652a1c12b2103eff9f
Amanda Jensen <jens2482@umn.edu>
2024-02-05 14:12:35 -0600

    changes from class

--- R/week3.R
+++ R/week3.R
@@ -3,10 +3,10 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 #Data Import and Cleaning
 raw_df <- read.csv('../data/week3.csv', header = TRUE)
-raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
-raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart, format = "%Y-%m-%d %H:%M:%S")
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd, format = "%Y-%m-%d %H:%M:%S")
 clean_df <- subset(raw_df, raw_df$timeStart >= "2017-07-01 00:00:00")
-clean_df <- subset(clean_df, q6 == "1")
+clean_df <- subset(clean_df, q6 == 1)
 
 #Analysis
 clean_df$timeSpent <- (difftime(clean_df$timeEnd, clean_df$timeStart, units = "secs"))
@@ -14,5 +14,5 @@ hist(clean_df$timeSpent <- as.numeric(clean_df$timeSpent)) #is there another way
 frequency_tables_list <- lapply(clean_df[, 5:14], table)
 lapply(frequency_tables_list, barplot)
 sum(clean_df$q1 >= clean_df$q2 & clean_df$q2 != clean_df$q3) #is this using coercion?
-for(freq in frequency_tables_list){
-  barplot(freq)}
+for(i in frequency_tables_list){
+  barplot(i)}

19e0e1e78e96355b6be0625f2e77d716ad9e90a7
Amanda Jensen <jens2482@umn.edu>
2024-02-04 23:59:24 -0600

    Part 2 complete; come back to #8...

--- R/week3.R
+++ R/week3.R
@@ -1,2 +1,10 @@
 #Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+#Data Import and Cleaning
+raw_df <- read.csv('../data/week3.csv', header = TRUE)
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart) 
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
+clean_df <- raw_df[7:49,] #I couldn't figure out how to make it work with dates...
+clean_df <- subset(clean_df, q6 == "1")
+
